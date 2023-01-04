77b51d73fe77f6c9a1c430cc7a2e5222403d142d
Alex Chaus <chausme@gmail.com>
2023-01-04 10:17:31 +0400

    Add Chats page placeholders Round 3

--- src/pages/chats/modules/chats/chats.js
+++ src/pages/chats/modules/chats/chats.js
@@ -1,3 +1,4 @@
 import template from './chats.hbs';
+import './chats.css';
 
 export default (props = {}) => template(props);
--- src/pages/chats/modules/single-chat/single-chat.js
+++ src/pages/chats/modules/single-chat/single-chat.js
@@ -1,3 +1,4 @@
 import template from './single-chat.hbs';
+import './single-chat.css';
 
 export default (props = {}) => template(props);
