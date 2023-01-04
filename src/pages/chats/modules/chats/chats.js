c3e4d51c691e434effa4d0cf393e45d0de8e025b
Alex Chaus <chausme@gmail.com>
2023-01-04 11:40:14 +0400

    Add Chats list output Round 1

--- src/pages/chats/modules/chats/chats.js
+++ src/pages/chats/modules/chats/chats.js
@@ -1,4 +1,29 @@
+import chat from './components/chat';
 import template from './chats.hbs';
 import './chats.css';
 
-export default (props = {}) => template(props);
+export default (
+    props = {
+        chats: [
+            chat({
+                avatar: 'https://via.placeholder.com/60',
+                title: 'Jake',
+                lastMessage:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscingelit. Nam mollis efficitur commodo. Cras venenatis...',
+                lastMessageType: 'text',
+                lastMessageOwner: 'them',
+                datetime: '2:14pm',
+                unread: 2,
+            }),
+            chat({
+                avatar: 'https://via.placeholder.com/60',
+                title: 'Kate',
+                lastMessage: 'Curabitur posuere ipsum nec orc!',
+                lastMessageType: 'text',
+                lastMessageOwner: 'you',
+                datetime: '10:10am',
+                unread: 1,
+            }),
+        ],
+    }
+) => template(props);
