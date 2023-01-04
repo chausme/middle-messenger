9918b4e9b5cfe02fce8600963d4eb1750d137d78
Alex Chaus <chausme@gmail.com>
2023-01-04 09:36:49 +0400

    Add Chats page placeholders Round 1

--- src/pages/chats/index.js
+++ src/pages/chats/index.js
@@ -1,18 +1,6 @@
-import chat from './modules/chat'; // single chat module with title and description output
+import chats from './modules/chats';
+import singleChat from './modules/single-chat';
 
 export default () => {
-    // @test data i.e. from API
-    const chats = [
-        { title: 123, desc: 'Lorem' },
-        { title: 321, desc: 'Ipsum' },
-        { title: 232, desc: 'Idolor' },
-    ];
-
-    return `
-        <h1>Heads up, this is a chat list</h1>
-        <h3>Number of chats is ${chats.length}</h3>
-        <div class="chats">
-            ${chats.map(item => chat(item)).join('')}
-        </div>
-    `;
+    return chats() + singleChat();
 };
--- src/pages/chats/modules/chat/index.js
+++
@@ -1,2 +0,0 @@
-export default data =>
-    `<div class="window bg-pink mb-3"><div class="inner">Chat title: ${data.title}, chat desc: ${data.desc}</div></div>`;
