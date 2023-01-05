93aaf0e519f0c123836134656ec472f814fd7311
Alex Chaus <chausme@gmail.com>
2023-01-05 13:17:39 +0400

    Chats page output Round 7

--- src/pages/chats/modules/single-chat/single-chat.js
+++ src/pages/chats/modules/single-chat/single-chat.js
@@ -1,5 +1,7 @@
 import header from './components/header';
 import empty from './components/empty';
+import message from './components/message';
+import date from './components/date';
 import footer from './components/footer';
 import template from './single-chat.hbs';
 import './single-chat.css';
@@ -8,13 +10,42 @@ const data = {
     title: 'Jerry',
 };
 
+/** @todo refactor when get data from API */
 export default (
     props = {
         header: header({
             avatar: 'https://via.placeholder.com/60',
             title: data.title,
         }),
-        empty,
+        date: date({
+            date: 'Jan 20',
+        }),
+        messages: [
+            {
+                message: message({
+                    content:
+                        'Pellentesque congue imperdiet urna non eleifend. In eu odio finibus pretium nisl non, gravida nunc. Cras faucibus in arcu et pulvinar. Mauris tincidunt accumsan convalli.<br /><br />Curabitur posuere ipsum nec orci auctor blandit. Nam at odio elementum, hendrerit ex aliquet, congue elit. Praesent tincidunt vestibulum lacinia. Nunc vehicula at mauris a consectetur.<br /><br />Vestibulum imperdiet tortor mauris, eu cursus lacus dignissim.',
+                    datetime: '2:14pm',
+                    styles: 'bg-green-light',
+                }),
+            },
+            {
+                message: message({
+                    content:
+                        'Pellentesque congue imperdiet urna non eleifend. In eu odio finibus pretium nisl non, gravida nunc. Cras faucibus in arcu et pulvinar. Mauris tincidunt accumsan convalli.<br /><br />Curabitur posuere ipsum nec orci auctor blandit. Nam at odio elementum, hendrerit ex aliquet, congue elit. Praesent tincidunt vestibulum lacinia. Nunc vehicula at mauris a consectetur.<br /><br />Vestibulum imperdiet tortor mauris, eu cursus lacus dignissim.',
+                    datetime: '2:14pm',
+                    styles: 'bg-green-light',
+                }),
+            },
+            {
+                message: message({
+                    content:
+                        'Pellentesque congue imperdiet urna non eleifend. In eu odio finibus pretium nisl non, gravida nunc. Cras faucibus in arcu et pulvinar. Mauris tincidunt accumsan convalli.<br /><br />Curabitur posuere ipsum nec orci auctor blandit. Nam at odio elementum, hendrerit ex aliquet, congue elit. Praesent tincidunt vestibulum lacinia. Nunc vehicula at mauris a consectetur.<br /><br />Vestibulum imperdiet tortor mauris, eu cursus lacus dignissim.',
+                    datetime: '2:14pm',
+                    styles: 'bg-green-light',
+                }),
+            },
+        ],
         footer,
     }
 ) => template({ props });
