0d1d8f4d14e71f2b21c5eef36f4701cd78d5c222
Alex Chaus <chausme@gmail.com>
2023-01-05 17:16:09 +0400

    Chats page output Round 8

--- src/pages/chats/modules/single-chat/single-chat.js
+++ src/pages/chats/modules/single-chat/single-chat.js
@@ -1,6 +1,7 @@
 import header from './components/header';
 import empty from './components/empty';
 import message from './components/message';
+import messageImage from './components/message-image';
 import date from './components/date';
 import footer from './components/footer';
 import template from './single-chat.hbs';
@@ -30,18 +31,16 @@ export default (
                 }),
             },
             {
-                message: message({
-                    content:
-                        'Pellentesque congue imperdiet urna non eleifend. In eu odio finibus pretium nisl non, gravida nunc. Cras faucibus in arcu et pulvinar. Mauris tincidunt accumsan convalli.<br /><br />Curabitur posuere ipsum nec orci auctor blandit. Nam at odio elementum, hendrerit ex aliquet, congue elit. Praesent tincidunt vestibulum lacinia. Nunc vehicula at mauris a consectetur.<br /><br />Vestibulum imperdiet tortor mauris, eu cursus lacus dignissim.',
-                    datetime: '2:14pm',
-                    styles: 'bg-green-light',
+                message: messageImage({
+                    image: 'https://via.placeholder.com/300x200',
+                    datetime: '2:18pm',
                 }),
             },
             {
                 message: message({
                     content:
                         'Pellentesque congue imperdiet urna non eleifend. In eu odio finibus pretium nisl non, gravida nunc. Cras faucibus in arcu et pulvinar. Mauris tincidunt accumsan convalli.<br /><br />Curabitur posuere ipsum nec orci auctor blandit. Nam at odio elementum, hendrerit ex aliquet, congue elit. Praesent tincidunt vestibulum lacinia. Nunc vehicula at mauris a consectetur.<br /><br />Vestibulum imperdiet tortor mauris, eu cursus lacus dignissim.',
-                    datetime: '2:14pm',
+                    datetime: '2:22pm',
                     styles: 'bg-green-light',
                 }),
             },
