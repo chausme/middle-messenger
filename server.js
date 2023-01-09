b8646122af393263b7b675b58ea248887f6ae00a
Alex Chaus <chausme@gmail.com>
2023-01-09 14:17:09 +0400

    Update server.js port to fetch from process.env

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const PORT = 3000;
 
 app.use(express.static('./dist'));
 
-app.listen(PORT, function () {
-    console.log(`Middle Messenger is up at http://localhost:${PORT}`);
+app.listen(process.env.PORT || PORT, function () {
+    console.log(`Middle Messenger is up at http://localhost:${process.env.PORT || PORT}`);
 });
