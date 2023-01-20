941ef5f3ff6fad97c90d1657b8b46e0ee99410e0
Alex Chaus <chausme@gmail.com>
2023-01-20 11:49:26 +0400

    Add Auth component Round 1

---
+++ src/pages/home/modules/auth/auth.ts
@@ -0,0 +1,36 @@
+import { inputWLabel } from '~/src/components/input-w-label';
+import { Button } from '~/src/components/button';
+import template from './signin.hbs';
+
+// @todo add sign in logic
+
+export default (
+    props = {
+        inputs: {
+            login: inputWLabel({
+                title: 'Login',
+                id: 'login',
+                type: 'text',
+            }),
+            password: inputWLabel({
+                title: 'Password',
+                id: 'password',
+                type: 'password',
+            }),
+        },
+        buttons: {
+            signin: button({
+                title: 'Sign In',
+                id: 'signin',
+                styles: 'mb-2 bg-green',
+                link: '',
+            }),
+            signup: button({
+                title: 'Sign Up',
+                id: 'test',
+                styles: 'bg-orange',
+                link: '#signup',
+            }),
+        },
+    }
+) => template({ props });
