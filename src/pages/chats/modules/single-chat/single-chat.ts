af2b4ce629be363a56f987c2edc4f66c7b52cd20
Alex Chaus <chausme@gmail.com>
2023-01-27 13:54:17 +0400

    Refactor Single chat messages into an array

--- src/pages/chats/modules/single-chat/single-chat.ts
+++ src/pages/chats/modules/single-chat/single-chat.ts
@@ -38,30 +38,32 @@ export default class SingleChat extends Block {
         this.children.datetime = new Datetime({
             date: 'Jun 20',
         });
-        this.children.message_1 = new Message({
-            content:
-                'Pellentesque congue imperdiet urna non eleifend. In eu odio finibus pretium nisl non, gravida nunc. Cras faucibus in arcu et pulvinar. Mauris tincidunt accumsan convalli.<br /><br />Curabitur posuere ipsum nec orci auctor blandit. Nam at odio elementum, hendrerit ex aliquet, congue elit. Praesent tincidunt vestibulum lacinia. Nunc vehicula at mauris a consectetur.<br /><br />Vestibulum imperdiet tortor mauris, eu cursus lacus dignissim.',
-            datetime: '2:14pm',
-        });
-        this.children.message_2 = new MessageImage({
-            image: imageMessage,
-            datetime: '2:18pm',
-        });
-        this.children.message_3 = new Message({
-            content: 'Cool!',
-            datetime: '2:20pm',
-            own: true,
-        });
-        this.children.message_4 = new MessageSticker({
-            sticker: 'smile',
-            datetime: '2:21pm',
-            own: true,
-        });
-        this.children.message_5 = new MessageImage({
-            image: imageMessage,
-            datetime: '2:30pm',
-            own: true,
-        });
+        this.children.messages = [
+            new Message({
+                content:
+                    'Pellentesque congue imperdiet urna non eleifend. In eu odio finibus pretium nisl non, gravida nunc. Cras faucibus in arcu et pulvinar. Mauris tincidunt accumsan convalli.<br /><br />Curabitur posuere ipsum nec orci auctor blandit. Nam at odio elementum, hendrerit ex aliquet, congue elit. Praesent tincidunt vestibulum lacinia. Nunc vehicula at mauris a consectetur.<br /><br />Vestibulum imperdiet tortor mauris, eu cursus lacus dignissim.',
+                datetime: '2:14pm',
+            }),
+            new MessageImage({
+                image: imageMessage,
+                datetime: '2:18pm',
+            }),
+            new Message({
+                content: 'Cool!',
+                datetime: '2:20pm',
+                own: true,
+            }),
+            new MessageSticker({
+                sticker: 'smile',
+                datetime: '2:21pm',
+                own: true,
+            }),
+            new MessageImage({
+                image: imageMessage,
+                datetime: '2:30pm',
+                own: true,
+            }),
+        ];
         this.children.form = new FormMessage({
             id: 'send-message',
             events: {
