746b9e340eaffeecda41a2c777c1dd087bdfffb7
Alex Chaus <chausme@gmail.com>
2023-02-15 09:40:39 +0400

    Add Messages POC Round 1

--- src/api/messages-api.ts
+++ src/api/messages-api.ts
@@ -3,7 +3,7 @@ const messagesAPIBase = 'WS';
 export class MessagesAPI {
     static basePath = 'WS';
 
-    connect() {
-        return 'something from ws';
+    connect(chatId: number, token: string) {
+        return `connecting to chatId:${chatId}, token:${token}`;
     }
 }
--- src/controllers/messages-controller.ts
+++ src/controllers/messages-controller.ts
@@ -1,13 +1,32 @@
 import { MessagesAPI } from '../api/messages-api';
+import { ChatsAPI } from '../api/chats-api';
 import store from '~/src/utils/store';
 
 export class MessagesController {
     #api = new MessagesAPI();
+    #chatsApi = new ChatsAPI();
 
-    async connect() {
+    async #getChatToken(chatId: number) {
+        const response = (await this.#chatsApi.getChatToken(chatId)) as XMLHttpRequest;
+        /** @todo add common function */
+        const responseText = JSON.parse(response.response);
+        if (response.status !== 200) {
+            const { reason } = responseText;
+            console.warn(`Oops, something went wrong: ${reason}`);
+            alert(`Oops, something went wrong: ${reason}`);
+            return;
+        }
+        if (!responseText.token) {
+            console.warn(`Oops, there is no chat token`);
+            alert(`Oops, there is no chat token`);
+            return;
+        }
+        return responseText.token;
+    }
+
+    async connect(chatId: number, token: string) {
         try {
-            const data = this.#api.connect();
-            console.log(data);
+            const data = (await this.#api.connect(chatId, token)) as XMLHttpRequest;
             return data;
         } catch (e: any) {
             alert(`Oops, something went wrong: ${e.message}`);
@@ -15,15 +34,27 @@ export class MessagesController {
         }
     }
 
-    async loadMessages() {
+    async loadMessages(chatId: number) {
         try {
+            // get chat token
+            const token = await this.#getChatToken(chatId);
+
+            if (!token) {
+                console.warn(`Oops, there is no chat token`);
+                alert(`Oops, there is no chat token`);
+                return;
+            }
+            // establish connection
+            const connection = await this.connect(chatId, token);
+            console.log(connection);
+            return;
+
             /** @todo replace with state management to avoid oldschool frontend override */
             const messagesWrap = document.querySelector('.messages');
             if (!messagesWrap) {
                 return;
             }
             messagesWrap.innerHTML = '';
-            await this.connect();
         } catch (e: any) {
             alert(`Oops, something went wrong: ${e.message}`);
             console.error(e.message);
