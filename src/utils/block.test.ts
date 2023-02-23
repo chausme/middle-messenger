191cfea0923812db57ef8e256a0eea446421664d
Alex Chaus <chausme@gmail.com>
2023-02-23 11:33:56 +0400

    Add basic component test Round 3

--- src/utils/block.test.ts
+++ src/utils/block.test.ts
@@ -1,31 +1,27 @@
 /**
  * @jest-environment jsdom
  */
-// import Block from './block';
+import Block from './block';
 
 describe('Component', () => {
-    // const testBlock = new (class BlankPage extends Block {
-    //     constructor() {
-    //         super({}, 'div');
-    //     }
-
-    //     render() {
-    //         const fragment = document.createDocumentFragment();
-    //         const p = document.createElement('p');
-    //         p.textContent = 'Test output';
-    //         fragment.appendChild(p);
-    //         return fragment;
-    //     }
-    // })();
-
-    test('should render content string', () => {
-        const div = document.createElement('div');
-        const p = document.createElement('p');
-        p.innerText = '123';
-        div.appendChild(p);
-        if (!div) {
-            return;
+    const testBlock = new (class BlankPage extends Block {
+        constructor() {
+            super({}, 'div');
         }
-        console.log(div.querySelector('p')?.innerText);
+
+        render() {
+            const fragment = document.createDocumentFragment();
+            const p = document.createElement('p');
+            p.innerText = 'Test output';
+            fragment.appendChild(p);
+            return fragment;
+        }
+    })();
+
+    test('should render fragment with correct contents', () => {
+        const fragment = testBlock.render();
+        const div = document.createElement('div');
+        div.appendChild(fragment);
+        expect(div.querySelector('p')?.innerText).toBe('Test output');
     });
 });
