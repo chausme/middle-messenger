02d2e236f4b5c2596f2fba25a2dd46f5831c3bc0
Alex Chaus <chausme@gmail.com>
2023-02-23 12:50:34 +0400

    Add basic Router test Round 1

---
+++ src/utils/router.test.ts
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+// import Router from './router2';
+
+describe('Router', () => {
+    test('should return router instance', () => {
+        const placeholder = 'Hello';
+        expect(placeholder).toBe('Hello');
+        // console.log(process.env);
+        // const routerTest = new Router({});
+        // console.log(routerTest);
+    });
+});
