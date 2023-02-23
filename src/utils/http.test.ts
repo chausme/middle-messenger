0b7617100d439bafa3ac4350afeced02c7f92fff
Alex Chaus <chausme@gmail.com>
2023-02-23 11:03:21 +0400

    Add basic HTTP test

---
+++ src/utils/http.test.ts
@@ -0,0 +1,14 @@
+import HTTP from '../utils/http';
+
+describe('HTTP module', () => {
+    const testApi = new HTTP();
+    console.log(testApi);
+    test('check for inner functions', () => {
+        expect(testApi).toMatchObject({
+            get: expect.any(Function),
+            post: expect.any(Function),
+            put: expect.any(Function),
+            delete: expect.any(Function),
+        });
+    });
+});
