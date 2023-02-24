aaf6aaa58d0a6105e05cb4bef37dd90d06460a72
Alex Chaus <chausme@gmail.com>
2023-02-24 15:54:47 +0400

    Add extra Router test

--- src/utils/router.test.ts
+++ src/utils/router.test.ts
@@ -9,4 +9,13 @@ describe('Router', () => {
         const routerTest = new Router({ blank: new BlankPage() });
         expect(routerTest).toBeInstanceOf(Router);
     });
+
+    test('should return correct window history length', () => {
+        const routerTest = new Router({ blank: new BlankPage() });
+        routerTest.init();
+        routerTest.load('blank');
+        routerTest.load('something');
+        routerTest.load('blank');
+        expect(window.history.length).toEqual(4);
+    });
 });
