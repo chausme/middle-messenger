c05ca32af97e2e81f486754118604bd4e21b380f
Alex Chaus <chausme@gmail.com>
2023-02-24 16:46:57 +0400

    Add extra component test

--- src/utils/block.test.ts
+++ src/utils/block.test.ts
@@ -4,24 +4,41 @@
 import Block from './block';
 
 describe('Component', () => {
-    const testBlock = new (class BlankPage extends Block {
-        constructor() {
-            super({}, 'div');
-        }
-
-        render() {
-            const fragment = document.createDocumentFragment();
-            const p = document.createElement('p');
-            p.innerText = 'Test output';
-            fragment.appendChild(p);
-            return fragment;
-        }
-    })();
-
     test('should render fragment with correct contents', () => {
-        const fragment = testBlock.render();
+        class BlankPage extends Block {
+            constructor() {
+                super({}, 'div');
+            }
+
+            render() {
+                const fragment = document.createDocumentFragment();
+                const p = document.createElement('p');
+                p.innerText = 'Test output';
+                fragment.appendChild(p);
+                return fragment;
+            }
+        }
+        const fragment = new BlankPage().render();
         const div = document.createElement('div');
         div.appendChild(fragment);
         expect(div.querySelector('p')?.innerText).toBe('Test output');
+    });
+
+    test('should return HTML Button element', () => {
+        class BlankButton extends Block {
+            constructor() {
+                super({}, 'button');
+            }
+
+            render() {
+                const fragment = document.createDocumentFragment();
+                const p = document.createElement('p');
+                p.innerText = 'Test output';
+                fragment.appendChild(p);
+                return fragment;
+            }
+        }
+        const button = new BlankButton();
+        expect(button.getContent()).toBeInstanceOf(HTMLButtonElement);
     });
 });
